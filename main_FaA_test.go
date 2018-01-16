4c80f88fefd9558de51fa111a94ddf917f1e6b4f
James K <james@gtoet.com>
2018-01-16 20:18:32 +1300

    Improved tests

--- main_FaA_test.go
+++ main_FaA_test.go
@@ -2,10 +2,49 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func generateList(n int) []int {
+	list := make([]int, n)
+	for i := 0; i < n; i++ {
+		list[i] = rand.Int()
+	}
+	return list
+}
+
+func Test_selectTopFaA_random(t *testing.T) {
+	n := 10 * 1000
+	b := 100
+	top := 100
+
+	list := generateList(n)
+
+	k := selectTopFaA(list, top, b)
+
+	assert.Equal(t, top, k)
+
+	actual := map[int]int{}
+	for i := 0; i < top; i++ {
+		v := list[i]
+		count := actual[v]
+		actual[v] = count + 1
+	}
+
+	sort.Ints(list)
+	expected := map[int]int{}
+	for i := 0; i < top; i++ {
+		v := list[i]
+		count := expected[v]
+		expected[v] = count + 1
+	}
+
+	assert.Equal(t, expected, actual)
+}
 
 func Test_partitionParallel_allthesame(t *testing.T) {
 	list := []int{2, 4, 7, 3, 1, 9, 2, 2, 5, 2, 4}
@@ -66,84 +105,86 @@ func Test_partition(t *testing.T) {
 
 func TestTakeLeftRight(t *testing.T) {
 	//Don't use zeros in the test list, tests assume 0 is an unset value in output
+	lists := [][]int{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}}
+	leftRightCombos := []struct {
+		left, right int
+	}{
+		{0, 9},
+	}
 	cases := []struct {
-		list        []int
 		takePattern []int
 	}{
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-1, 1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-1, 1, -1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-1, 1, -1, -1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{1, -1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{1, -1, 1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{1, -1, 1, 1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{1, -1, -1, 1},
 		},
 		{
-			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 			[]int{-1, 1, 1, -1},
 		},
 	}
 
 	for _, c := range cases {
-		for b := 1; b <= len(c.list); b++ {
-			t.Run(fmt.Sprintf("Running case %v with block size of %v", c.takePattern, b), func(t *testing.T) {
-				s := NewLeftRightSubLists(c.list, 0, len(c.list)-1, b)
+		for listI, list := range lists {
+			for _, leftRight := range leftRightCombos {
+				left := leftRight.left
+				right := leftRight.right
 
-				output := make([]int, len(c.list))
-				bLeft := s.TakeNextLeft()
-				bRight := s.TakeNextRight()
-				for bLeft != nil || bRight != nil {
-					if bLeft != nil {
-						for i := bLeft.beginIndex; i <= bLeft.endIndex; i++ {
-							if output[i] != 0 {
-								t.Errorf("Output at index %v has already been set! On left %v", i, bLeft)
-							}
-							output[i] = c.list[i]
-						}
-					}
-					if bRight != nil {
-						for i := bRight.beginIndex; i <= bRight.endIndex; i++ {
-							if output[i] != 0 {
-								t.Errorf("Output at index %v has already been set! On right %v", i, bRight)
-							}
-							output[i] = c.list[i]
-						}
-					}
+				for b := 1; b <= len(list); b++ {
+					t.Run(fmt.Sprintf("Running case %v with block size of %v with left %v and right %v for list at index %v", c.takePattern, b, left, right, listI), func(t *testing.T) {
+						s := NewLeftRightSubLists(list, left, right, b)
 
-					bLeft = s.TakeNextLeft()
-					bRight = s.TakeNextRight()
+						output := make([]int, len(list))
+						bLeft := s.TakeNextLeft()
+						bRight := s.TakeNextRight()
+						for bLeft != nil || bRight != nil {
+							if bLeft != nil {
+								for i := bLeft.beginIndex; i <= bLeft.endIndex; i++ {
+									if output[i] != 0 {
+										t.Errorf("Output at index %v has already been set! On left %v", i, bLeft)
+									}
+									output[i] = list[i]
+								}
+							}
+							if bRight != nil {
+								for i := bRight.beginIndex; i <= bRight.endIndex; i++ {
+									if output[i] != 0 {
+										t.Errorf("Output at index %v has already been set! On right %v", i, bRight)
+									}
+									output[i] = list[i]
+								}
+							}
+
+							bLeft = s.TakeNextLeft()
+							bRight = s.TakeNextRight()
+						}
+
+						assert.Equal(t, list, output, "Output list did not match input list")
+					})
 				}
-
-				assert.Equal(t, c.list, output, "Output list did not match input list")
-			})
+			}
 		}
 	}
 }
